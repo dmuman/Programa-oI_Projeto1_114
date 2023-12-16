7f7e5e875f6af093eb95bc64593f94b24c9ef0e9
dmuman <150830052+dmuman@users.noreply.github.com>
2023-12-16 23:40:05 +0000

    Update infoToFiles.py

--- projeto/infoToFiles.py
+++ projeto/infoToFiles.py
@@ -5,7 +5,10 @@
 # 59348 Dmytro Umanskyi 
 # 62263 Eduardo Rocha
 
-
+from constants import *
+from dateTime import *
+from planning import *
+from infoFromFiles import *
 
 def writeScheduleFile(sched, header, fileName):
     """
@@ -24,6 +27,23 @@ def writeScheduleFile(sched, header, fileName):
     the lines in this file keeps the ordering top to bottom of 
     the assistances as ordered head to tail in sched.
     """
+    header[HEADER_TIME_IDX][0] = updateHours(header[HEADER_TIME_IDX][0], 30)
+    #fileName = f"schedule{header[HEADER_TIME_IDX][0]}.txt"
+    outFile = open(fileName, "w", encoding="utf-8")
+    for headLine in header:
+        outFile.write(' '.join(headLine) + '\n')
+    for index, schedule in enumerate(sched):
+        if index >= len(sched) - 1:
+            outFile.write(', '.join(schedule))
+        else:
+            outFile.write(', '.join(schedule) + '\n')
+
+    outFile.close()
+
+newSchedule = updateSchedule("doctors16h00.txt", "requests16h30.txt", "schedule16h00.txt", "16h30")
+header = saveHeader("schedule16h00.txt")
+
+writeScheduleFile(newSchedule, header, "schedule160h30.txt")
 
 
 
